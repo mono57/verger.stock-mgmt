588e1085a57d33d86cd846dbf7277aaf983fea6a
mono57 <aymaramono@gmail.com>
2021-02-24 11:48:00 +0100

    redirect project root url to backoffice root url

--- stockmgmt/urls.py
+++ stockmgmt/urls.py
@@ -1,9 +1,12 @@
+from django.conf.urls import url
 from django.contrib.auth.views import LoginView
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('backoffice/', include('backoffice.urls', namespace='backoffice')),
+    path('', RedirectView.as_view(url='/backoffice')),
     path('login/', LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
 ]
