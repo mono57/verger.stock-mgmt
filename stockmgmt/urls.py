0b020efc8678ebc426e4198f23d112d492f070f6
mono57 <aymaramono@gmail.com>
2021-02-24 23:52:36 +0100

    add feature password reset

--- stockmgmt/urls.py
+++ stockmgmt/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView
 from django.views.generic.base import RedirectView
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,5 +9,7 @@ urlpatterns = [
     path('backoffice/', include('backoffice.urls', namespace='backoffice')),
     path('', RedirectView.as_view(url='/backoffice')),
     path('login/', LoginView.as_view(), name='login'),
+    path('passwords/change/', PasswordChangeView.as_view(), name='password_change_view'),
+    path('passwords/change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('admin/', admin.site.urls),
 ]
