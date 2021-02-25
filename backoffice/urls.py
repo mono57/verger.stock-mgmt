91df771158ddf7b24f1f36d9bb4aa28a51afd36b
mono57 <aymaramono@gmail.com>
2021-02-25 08:31:04 +0100

    add product list

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from backoffice.views import (
     ProductCreateView, 
-    DashboardTemplateView, RoomCreateView, RoomListView, RoomUpdateView)
+    DashboardTemplateView, ProductListView, 
+    RoomCreateView, RoomListView, RoomUpdateView)
 
 app_name = 'backoffice'
 
 urlpatterns = [
     path('', DashboardTemplateView.as_view(template_name='index.html'), name='home'),
     path('product/add/', ProductCreateView.as_view(), name='product-add'),
+    path('product/list/', ProductListView.as_view(), name='product-list'),
     path('room/add/', RoomCreateView.as_view(), name="room-add"),
     path('room/list/', RoomListView.as_view(), name="room-list"),
     path('room/<int:pk>/update/', RoomUpdateView.as_view(), name="room-update"),
